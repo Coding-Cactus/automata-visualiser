24cbe073fb095607ce5c082a738c4fc9318dd1da
Coding-Cactus <codingcactus.cc@gmail.com>
2025-11-11 13:58:34 +0000

    parametreise automata data type, and super basic svg output

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,81 +1,202 @@
-module MyLib (a1) where
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module MyLib (a1, render, svg) where
 
 import qualified Control.Monad.State as S
+import Data.Text (Text, unpack, pack, singleton)
+import qualified Data.Text.IO as T (writeFile)
 
-data Automaton = Automaton {
-  states :: [State],
-  transitions :: [Transition],
+import qualified Svg
+
+data Automaton s t = Automaton {
+  states :: [State s],
+  transitions :: [Transition s t],
   initialS :: Int,
   finalS :: [Int]
 } deriving Show
 
 
-type AutomatonBuilder = S.State Automaton ()
+type AutomatonBuilder s t = S.State (Automaton s t) ()
 
 
-data State = S Int String
+data State s where
+  S :: Label s => Int -> s -> State s
 
-instance Show State where
-  show (S _ name) = show name
-
-
-type Transition = (State, State, TransitionCondition)
+instance Show (State s) where
+  show (S _ name) = unpack $ drawLabel name
 
 
-data TransitionCondition = Simple Char | Stack Char (Char, Char)
+class Label a where
+  drawLabel :: a -> Text
 
-instance Show TransitionCondition where
-  show (Simple c) = show c
-  show (Stack c (s1, s2)) = show c <> ", " <> show s1 <> "->" <> show s2
+instance Label Char where
+  drawLabel = singleton
+
+instance Label String where
+  drawLabel = pack
+
+instance Label Text where
+  drawLabel = id
+
+instance Label Int where
+  drawLabel = pack . show
+
+instance Label Float where
+  drawLabel = pack . show
+
+instance Label Double where
+  drawLabel = pack . show
 
 
-class ToTransition a where
-  toTransition :: a -> TransitionCondition
+data Transition s t where
+  T :: TransitionLabel t => State s -> State s -> t -> Transition s t
 
-instance ToTransition Char where
-  toTransition = Simple
+instance Show (Transition s t) where
+  show (T s1 s2 t) = show (s1, s2, toTransition t)
 
-instance ToTransition TransitionCondition where
+
+
+class TransitionLabel a where
+  toTransition :: a -> Text
+
+instance TransitionLabel Char where
+  toTransition = singleton
+
+instance TransitionLabel String where
+  toTransition = pack
+
+instance TransitionLabel Text where
   toTransition = id
+
+instance TransitionLabel Int where
+  toTransition = pack . show
+
+instance TransitionLabel Float where
+  toTransition = pack . show
+
+instance TransitionLabel Double where
+  toTransition = pack . show
+
+
+data StackTransition a b where
+  StackT :: (Label a, Label b) => a -> (b, b) -> StackTransition a b
+
+instance TransitionLabel (StackTransition a b) where
+  toTransition (StackT token (stack1, stack2)) = drawLabel token <> ", " <> drawLabel stack1 <> "->" <> drawLabel stack2
+
 
 
 infixl 5 >-|
-(>-|) :: ToTransition t => State -> t -> (State, TransitionCondition)
-s >-| cond = (s, toTransition cond)
+(>-|) :: State s -> t -> (State s, t)
+s >-| condition = (s, condition)
 
 infixl 5 |->
-(|->) :: (State, TransitionCondition) -> State -> S.State Automaton ()
-(s1, c) |-> s2 = S.modify (\a -> a { transitions = (s1, s2, c) : transitions a })
+(|->) :: TransitionLabel t => (State s, t) -> State s -> S.State (Automaton s t) ()
+(s1, c) |-> s2 = S.modify (\a -> a { transitions = T s1 s2 c : transitions a })
 
 infixl 6 ~~
-(~~) :: Char -> (Char, Char) -> TransitionCondition
-(~~)  = Stack
+(~~) :: (Label t, Label w) => t -> (w, w) -> StackTransition t w
+(~~) = StackT
 
 
-state :: String -> S.State Automaton State
-state x = do
+state :: Label s => s -> S.State (Automaton s t) (State s)
+state name = do
   a <- S.get
-  let newS = S (length $ states a) x
+  let newS = S (length $ states a) name
   S.put $ a { states = newS : states a }
   pure newS
 
 
-initial :: State -> AutomatonBuilder
+initial :: State s -> AutomatonBuilder s t
 initial (S sid _) = S.modify $ \a -> a { initialS = sid }
 
-final :: State -> AutomatonBuilder
+final :: State s -> AutomatonBuilder s t
 final (S sid _) = S.modify $ \a -> a { finalS = sid : finalS a }
 
 
-a1 :: AutomatonBuilder
+
+
+data PositionedState = PS {
+  sLabel :: Text,
+  x :: Int,
+  y :: Int,
+  isInitial :: Bool,
+  isFinal :: Bool
+}
+
+data PositionedTranistions = PT {
+  tLabel :: Text,
+  startX :: Int,
+  startY :: Int,
+  endX :: Int,
+  endY :: Int
+}
+
+data AutomatonLayout = AL {
+  positionedStates :: [PositionedState],
+  positionedTransitions :: [PositionedTranistions]
+}
+
+
+layout :: Automaton s t -> AutomatonLayout
+layout a = AL {
+    positionedStates = map positionState $ states a,
+    positionedTransitions = []
+  }
+  where
+    positionState (S sid name) = PS {
+      sLabel = drawLabel name,
+      x = sid,
+      y = 0,
+      isInitial = sid == initialS a,
+      isFinal = sid `elem` finalS a
+    }
+
+
+data AutomatonRender = TextData Text | BinaryData Text
+
+svgStateGap :: Int
+svgStateRadius :: Int
+svgAcceptStateRadius :: Int
+svgPositionScale :: Int
+svgStateGap = 30
+svgStateRadius = 25
+svgAcceptStateRadius = svgStateRadius + 3
+svgPositionScale = svgStateGap + svgStateRadius*2
+
+svg :: AutomatonLayout -> AutomatonRender
+svg (AL sts _) = TextData $ Svg.render $ Svg.Svg $ concatMap drawStates sts
+  where
+    drawStates (PS name xPos yPos _ isF) = [Svg.Circle scaleX scaleY svgStateRadius,
+                                            Svg.Text scaleX scaleY name]
+                                            <> outerCircle
+      where
+        scaleX = xPos * svgPositionScale
+        scaleY = yPos * svgPositionScale
+        outerCircle = if isF then [Svg.Circle scaleX scaleY svgAcceptStateRadius] else mempty
+
+saveToFile :: String -> AutomatonRender -> IO ()
+saveToFile fname = \case
+  TextData txt -> T.writeFile fname txt
+  BinaryData _ -> undefined
+
+render :: String -> (AutomatonLayout -> AutomatonRender) -> AutomatonBuilder s t -> IO ()
+render file fn a = saveToFile file $ fn $ layout $ S.execState a (Automaton [] [] (-1) [])
+
 a1 = do
-  a <- state "a"
-  a' <- state "a"
-  b <- state "b"
+  a <- state 'a'
+  a' <- state 'b'
+  b <- state 'c'
+  --_ <- state "c"
+  --_ <- state "abc"
+  --_ <- state "hi"
 
   initial a
   final a
   final b
 
   a >-|'a'|-> b
-  a >-|'a' ~~ ('a', 'b')|-> b
+  --a >-|(0::Int) ~~ ('a', 'b')|-> b
+  a >-|'b'|-> b
---
+++ src/Svg.hs
@@ -0,0 +1,58 @@
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Svg (SVG(Svg, Circle, Text), render) where
+
+import Data.Foldable (foldl')
+import qualified Data.Text as T
+
+data BoundingBox a where
+  BB :: (Show a, Num a, Ord a) => {
+    xmin :: a,
+    xmax :: a,
+    ymin :: a,
+    ymax :: a
+  } -> BoundingBox a
+
+data SVG a where
+  Svg    :: (Show a, Num a, Ord a) => [SVG a] -> SVG a
+  Text   :: (Show a, Num a, Ord a) => a -> a -> T.Text -> SVG a -- Text x y content
+  Circle :: (Show a, Num a, Ord a) => a -> a -> a -> SVG a      -- Circle x y radius
+
+render :: SVG a -> T.Text
+render (Svg elems) = let (BB minX maxX minY maxY) = boundingBox $ Svg elems in
+  "<svg "
+  <> "viewBox=\"" <> T.intercalate " " (map (T.pack . show) [minX, minY, maxX - minX, maxY - minY])
+  <> "\" xmlns=\"http://www.w3.org/2000/svg\">"
+  <> T.concat (map render elems)
+  <> "</svg>"
+render (Text x y txt) = let (x', y') = mapPair (T.pack . show) (x, y) in
+  "<text "
+  <> "text-anchor=\"middle\" dominant-baseline=\"middle\" "
+  <> "x=\"" <> x' <> "\" y=\"" <> y'
+  <> "\">" <> txt <> "</text>"
+render (Circle x y r) = let (x', y', r') = mapTriple (T.pack . show) (x, y, r) in
+  "<circle "
+  <> "cx=\"" <> x' <> "\" cy=\"" <> y' <> "\" r=\"" <> r'
+  <> "\" stroke=\"black\" fill=\"none\" />"
+
+boundingBox :: SVG a -> BoundingBox a
+boundingBox (Svg []) = BB 0 0 0 0
+boundingBox (Svg elems) = foldl' findBox initBox $ map boundingBox (tail elems)
+  where
+    findBox (BB minX maxX minY maxY) (BB minX' maxX' minY' maxY') = BB {
+      xmin = min minX minX',
+      xmax = max maxX maxX',
+      ymin = min minY minY',
+      ymax = max maxY maxY'
+    }
+    initBox = boundingBox $ head elems
+boundingBox (Text x y _) = BB x x y y
+boundingBox (Circle x y r) = BB (x-r) (x+r) (y-r) (y+r)
+
+
+
+mapPair :: (a -> b) -> (a, a) -> (b, b)
+mapPair f (x, y) = (f x, f y)
+mapTriple :: (a -> b) -> (a, a, a) -> (b, b, b)
+mapTriple f (x, y, z) = (f x, f y, f z)
